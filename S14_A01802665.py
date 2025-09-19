d854186e0cf3b22cfdc8b6fa2c880efdc06b2f92
a01802665-byte <a01802665@tec.mx>
2025-09-19 07:56:43 -0600

    Create S14_A01802665.py

---
+++ S14_A01802665.py
@@ -0,0 +1,13 @@
+# Write a python script that asks the user for their age and determines if they are adult or not (< 18 years old).
+age = int(input("What's your age?: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
+# Write a python script that asks the user for their age and determines if they are a teenager or not (13-19 years old).
+age = int(input("What's your age?: "))
+if 13 <= age <= 19:
+    print("You are a teenager.")
+else:
+    print("You are not a teenager.")
+  
